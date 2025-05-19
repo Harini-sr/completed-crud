d94c517f88678c4425b9bb46b4e7f36587d77c54
Harini-sr <harivarshini.sr@gmail.com>
2025-05-19 10:16:46 +0530

    Updated

--- server/server.js
+++ server/server.js
@@ -3,22 +3,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const PORT = 4500;
+const PORT = process.env.PORT || 4500;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/userDetails')
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected successfully"))
+.catch((error) => console.error("MongoDB connection error:", error));
+
 
 
 const employeeSchema = new mongoose.Schema({
